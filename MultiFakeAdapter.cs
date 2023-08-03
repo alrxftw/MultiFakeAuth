9a2fbbfea7fafb73a5b1fbffa16668cc8d09e3c2
Alex <Alex@DESKTOP-R6E0PO4>
2023-08-03 13:28:23 +0100

    Add project files.

---
+++ MultiFakeAdapter.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Globalization;
+using System.IO;
+using System.Net;
+using System.Xml.Serialization;
+using Microsoft.IdentityServer.Web.Authentication.External;
+using Claim = System.Security.Claims.Claim;
+
+namespace MultiFakeAuth
+{
+    class Adapter : IAuthenticationAdapter
+    {
+        static bool ValidateProofData(IProofData proofData, IAuthenticationContext authContext)
+        {
+            if (proofData == null || proofData.Properties == null || !proofData.Properties.ContainsKey("ChallengeQuestionAnswer"))
+            {
+                throw new ExternalAuthenticationException("Error - no answer found", authContext);
+            }
+
+            if ((string)proofData.Properties["ChallengeQuestionAnswer"] == "true")
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        public IAuthenticationAdapterMetadata Metadata
+        {
+            get { return new Metadata(); }
+        }
+
+        public IAdapterPresentation BeginAuthentication(Claim identityClaim, HttpListenerRequest request, IAuthenticationContext authContext)
+        {
+            return new PresentationForm();
+        }
+
+        public bool IsAvailableForUser(Claim identityClaim, IAuthenticationContext authContext)
+        {
+            return true; //its all available for now
+        }
+
+        public void OnAuthenticationPipelineLoad(IAuthenticationMethodConfigData configData)
+        {
+            //this is where AD FS passes us the config data, if such data was supplied at registration of the adapter
+        }
+
+        public void OnAuthenticationPipelineUnload()
+        {
+
+        }
+
+        public IAdapterPresentation OnError(HttpListenerRequest request, ExternalAuthenticationException ex)
+        {
+            return new PresentationForm();
+        }
+
+        public IAdapterPresentation TryEndAuthentication(IAuthenticationContext authContext, IProofData proofData, HttpListenerRequest request, out Claim[] outgoingClaims)
+        {
+            outgoingClaims = new Claim[0];
+            if (ValidateProofData(proofData, authContext))
+            {
+                //authn complete - return authn method
+                outgoingClaims = new[]
+                {
+            // Return the required authentication method claim, indicating the particulate authentication method used.
+            new Claim( "http://schemas.microsoft.com/ws/2008/06/identity/claims/authenticationmethod", "http://schemas.microsoft.com/ws/2008/06/identity/authenticationmethod/hardwaretoken" )
+        };
+                return null;
+            }
+            else
+            {
+                //authentication not complete - return new instance of IAdapterPresentationForm derived class
+                return new PresentationForm();
+            }
+        }
+
+    }
+}
---
+++ PresentationForm.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.IdentityServer.Web.Authentication.External;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Resources;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MultiFakeAuth
+{
+    public class PresentationForm : IAdapterPresentationForm
+    {
+        /// Returns the HTML Form fragment that contains the adapter user interface. This data will be included in the web page that is presented
+        /// to the cient.
+        public string GetFormHtml(int lcid)
+        {
+            string htmlTemplate = Reources.Resources.FormArea;
+            return htmlTemplate;
+        }
+
+        /// Return any external resources, ie references to libraries etc., that should be included in
+        /// the HEAD section of the presentation form html.
+        public string GetFormPreRenderHtml(int lcid)
+        {
+            return null;
+        }
+
+        //returns the title string for the web page which presents the HTML form content to the end user
+        public string GetPageTitle(int lcid)
+        {
+            return "MultiFakeAuth Adapter";
+        }
+    }
+}
